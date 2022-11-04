910c8b083835638b164d49e12aada9151d185dd5
ssr765 <64972770+ssr765@users.noreply.github.com>
2022-11-04 01:41:53 +0100

    Create etc.py

---
+++ etc.py
@@ -0,0 +1,14 @@
+import os
+
+def limpiar_pantalla():
+    """Limpia la pantalla con "cls" en caso de Windows, si no usa "clear"."""
+    os.system("cls") if os.name == "nt" else os.system("clear")
+
+def centrar_en_terminal(cadena: str) -> str:
+    """Devuelve la cadena centrada en la terminal."""
+    return cadena.center(os.get_terminal_size().columns)
+
+def enter() -> None:
+    """Pausa para que se vea el contenido en la pantalla."""
+    print()
+    input("Pulsa ENTER para volver al menú principal")
